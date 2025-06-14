5e080c2217c7b24d5f1ae9ac52d6f31af09d272e
A-nirud-h <1159anirudh@gmail.com>
2025-06-14 20:06:14 +0530

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,2 +1,17 @@
-print('helloworld')
-hhhhhhhh = int(input('hello')
+#print('helloworld')
+#hhhhhhhh = int(input('hello')
+
+import tkinter as tk
+window=tk.Tk()
+window.title("**** TRAVELS")
+window.geometry("1024x640")
+label = tk.Label(window, text="Choose the user", font=("Arial", 14))
+label.pack()
+def click():
+    label.config(text="Welcome!")
+bt1=tk.Button(window,text="Admin",command=click)
+bt1.pack()
+bt2=tk.Button(window,text="customer",command=click)
+bt2.pack()
+window.mainloop()
+
